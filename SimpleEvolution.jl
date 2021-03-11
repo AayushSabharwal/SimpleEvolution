0601dd6d7a00df719f7279d071f99179f02fdde9
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2021-03-11 19:52:00 +0530

    Added Food stepping, simplified initialization

--- SimpleEvolution.jl
+++ SimpleEvolution.jl
@@ -2,6 +2,7 @@ using Agents
 using Random
 
 const AXIAL_DIRECTIONS = [-1, 0, 1]
+const NEIGHBORHOOD = [a for a in Iterators.product([-1:1 for φ = 1:2]...) if a != (0, 0)]
 
 @agent Bacterium GridAgent{2} begin
     age::Int        # age, in iterations. They only live so long
@@ -22,12 +23,8 @@ function initialize_model(
     dims::Dims{2},  # size of the space
     food_distribution::Array{Float64,2};    # food at each cell
     n_bacteria::Int = 100,  # number of bacteria
-    energy::Float64 = 20.0, # initial energy of each bacteria
-    sensory_radius::Float64 = 4.0,  # initial sensory radius of each bacteria
-    threshold::Float64 = 50.0,  # initial reproduction threshold of each bacteria
-    speed::Int = 1, # initial speed of each bacteria
-    food_cap::Float64 = maximum(food_distribution), # initial food cap for all food
-    regen_rate::Float64 = 10.0, # initial regen rate for all food
+    initial_bacterium::Bacterium = Bacterium(-1, (-1, -1), 0, 20.0, 4.0, 50.0, 1, (-1, -1)),
+    initial_food::Food = Food(-1, (-1, -1), maximum(food_distribution), 0.0, 10.0),
     lifetime::Int = 200,    # initial lifetime for all bacteria
     eat_rate_factor::Float64 = 5.0, # how fast all bacteria eat
     sensory_radius_cost_factor::Float64 = 1.0,  # energy cost multiplier for sensory radius
@@ -36,18 +33,29 @@ function initialize_model(
     reproduction_threshold_std::Float64 = 1.0,  # random variation in inheriting reproduction threshold
     speed_std::Float64 = 2.0,   # random variation in inheriting speed
     reproduction_energy_cost::Float64 = 5.0,    # energy cost of reproduction
+    food_spread_coefficient::Float64 = 3.0,
+    food_reproduction_cost::Float64 = 10.0,
+    food_cap_std::Float64 = 5.0,
+    regen_rate_std::Float64 = 5.0,
 )
     # sanity checks
     @assert size(food_distribution) == dims
-    @assert food_cap >= maximum(food_distribution)
+    @assert initial_food.food_cap >= maximum(food_distribution)
     @assert lifetime > 0
 
     # reproducibility matters
     rng = MersenneTwister(42)
 
+    food_properties = Dict(
+        :spread_coefficient => food_spread_coefficient,
+        :reproduction_cost => food_reproduction_cost,
+        :food_cap_std => food_cap_std,
+        :regen_rate_std => regen_rate_std,
+    )
+
     # food lives in a separate model, since this improves performance
     food_space = GridSpace(dims; periodic = false)
-    food_model = ABM(Food, food_space; rng)
+    food_model = ABM(Food, food_space; properties = food_properties, rng)
 
     properties = Dict(
         :eat_rate_factor => eat_rate_factor,
@@ -67,14 +75,29 @@ function initialize_model(
 
     # add bacteria at random places
     for i ∈ 1:n_bacteria
-        add_agent!(Bacterium, model, 0, energy, sensory_radius, threshold, speed, (-1, -1))
+        add_agent!(
+            Bacterium,
+            model,
+            initial_bacterium.age,
+            initial_bacterium.energy,
+            initial_bacterium.sensory_radius,
+            initial_bacterium.reproduction_threshold,
+            initial_bacterium.speed,
+            (-1, -1),
+        )
     end
 
     # add food wherever it is nonzero
     for pos ∈ eachindex(food_distribution)
         food_distribution[pos] > 0 || continue
 
-        add_agent!(Food, model.food, food_cap, food_distribution[pos], regen_rate)
+        add_agent!(
+            Food,
+            model.food,
+            initial_food.food_cap,
+            food_distribution[pos],
+            initial_food.regen_rate,
+        )
     end
 
     return model
@@ -103,12 +126,12 @@ inherit(parameter::Int, rng::MersenneTwister, std::Float64) =
     max(parameter + floor(Int, randn(rng) * std), 1)
 
 """
-    reproduce(bact::Bacterium, model::ABM)
+    reproduce!(bact::Bacterium, model::ABM)
 
 Simulates reproduction of bacteria. Energy is halved after subtracting a cost. Inherited features are
 varied.
 """
-function reproduce(bact::Bacterium, model::ABM)
+function reproduce!(bact::Bacterium, model::ABM)
     add_agent!(
         bact.pos,
         Bacterium,
@@ -128,18 +151,18 @@ function reproduce(bact::Bacterium, model::ABM)
     bact.sensory_radius = inherit(bact.sensory_radius, model.rng, model.sensory_radius_std)
     bact.reproduction_threshold =
         inherit(bact.reproduction_threshold, model.rng, model.reproduction_threshold_std)
-    bact.speed = inherit(bact.speed_std, model.rng, model.speed_std)
+    bact.speed = inherit(bact.speed, model.rng, model.speed_std)
 end
 
 function agent_step!(bact::Bacterium, model::ABM)
-    # die, of age or startvation
+    # die of age or starvation
     (bact.age > model.lifetime || bact.energy <= 0) && (kill_agent!(bact, model); return)
 
     # grow older
     bact.age += 1
 
     # reproduce if possible
-    bact.energy >= bact.reproduction_threshold && reproduce(bact, model)
+    bact.energy >= bact.reproduction_threshold && reproduce!(bact, model)
 
     # if currently on a food source
     if !isempty(bact.pos, model.food)
@@ -200,3 +223,38 @@ function agent_step!(bact::Bacterium, model::ABM)
         model.sensory_radius_cost_factor * bact.sensory_radius +
         model.distance_cost_factor * max(abs.(movement)...)
 end
+
+"""
+    reproduce!(food::Food, pos::Dims{2}, model::ABM)
+
+Simulates reproduction of food. Inherited features are varied.
+"""
+function reproduce!(food::Food, pos::Dims{2}, model::ABM)
+    add_agent!(
+        pos,
+        Food,
+        model,
+        inherit(food.food_cap, model.rng, model.food_cap_std),
+        (food.current_food - model.reproduction_cost) / 2.0,
+        inherit(food.regen_rate, model.rng, model.regen_rate_std),
+    )
+
+    food.current_food = (food.current_food - model.reproduction_cost) / 2.0
+end
+
+@inline inbounds(pos::Dims{2}, dims::Dims{2}) = all(1.0 .<= pos .<= dims)
+
+function agent_step!(food::Food, model::ABM)
+    food.current_food <= 0 && (kill_agent!(food, model); return)
+
+    food.current_food = min(food.current_food + food.regen_rate, food.food_cap)
+
+    for offset in NEIGHBORHOOD
+        inbounds(food.pos .+ offset, size(model.space.s)) || continue
+        isempty(food.pos .+ offset, model) || continue
+        rand(model.rng) <= exp(-model.spread_coefficient * food.regen_rate) || continue
+        reproduce!(food, food.pos .+ offset, model)
+    end
+end
+
+food_step!(model::ABM) = Agents.step!(model.food, agent_step!)
