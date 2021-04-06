559fff91526cb498266a451ef96c613b1831b5e3
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2021-04-06 16:32:36 +0530

    plot_agent_scatter also does aggregate lines

--- plotter.jl
+++ plotter.jl
@@ -3,7 +3,7 @@ using GLMakie
 function plot_agg(agglog::String; suffixes = ["energy", "sens", "repr", "speed"])
     df = CSV.File(agglog) |> DataFrame
     nspecies = maximum(df.species)
-    gdf = groupby(df, :species)
+    gdf = groupby(df, :species; sort = true)
     fig = Figure(resolution = (1600, 1000))
     cols = cgrad(:lighttest; alpha=0.4)
     
@@ -37,20 +37,34 @@ function plot_food(foodlog::String)
     fig, food
 end
 
-function plot_agent_scatter(bactlog::String; params = ["sens"])
+function plot_agent_scatter(bactlog::String, agglog::String; params = ["sens", "repr", "speed"])
+    agg_df = CSV.File(agglog) |> DataFrame
+    n_species = maximum(agg_df.species)
+    agg_df = groupby(agg_df, :species; sort = true)
+    cols = cgrad(:RdYlGn_4; alpha=0.8)
+
+    plotline(ax, steps, data, color) = lines!(ax, steps, data; color, linewidth = 2)
+
     df = CSV.File(bactlog; select = ["step", "species", params...]) |> DataFrame
-    n_species = maximum(df.species)
     df = groupby(df, :species; sort = true)
     
-    sp_cols = cgrad(:tab20; alpha=0.1)
+    sp_cols = cgrad(:Spectral_6; alpha=0.1)
     fig = Figure(resolution = (600, 600))
+    
+    l1 = l2 = nothing
     for (ind, par) in enumerate(params)
         for i in 1:n_species
-            ax = fig[ind, i] = Axis(fig; ylabel = par)
+            ax = fig[1+ind, i] = Axis(fig; ylabel = par)
             xlims!(ax, (0, maximum(df[1].step)))
-            scatter!(ax, df[i].step, df[i][!, par]; color = sp_cols[(i-1)/n_species], markersize = 4, strokewidth = 0)
+            scatter!(ax, df[i].step, df[i][!, par]; color = sp_cols[i/n_species], markersize = 4, strokewidth = 0)
+            l1 = plotline(ax, agg_df[i].step, agg_df[i][!, "μ_$par"], cols[0.])
+            l2 = plotline(ax, agg_df[i].step, agg_df[i][!, "σ_$par"], cols[1.])
         end
     end
 
+    ax = fig[1, :] = Axis(fig; ylabel = "Bact population")
+    lines = [plotline(ax, agg_df[i].step, agg_df[i].nbact, (sp_cols[i/n_species], 1.0)) for i in 1:n_species]
+    
+    fig[end+1, :] = Legend(fig, [l1, l2], ["Mean", "Std"]; orientation = :horizontal)
     fig
 end
